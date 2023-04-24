df8f677f7c1434e2ef9609089b060d1f13f3d1b5
Abhishek Kumar <abhishekkumar.in047@gmail.com>
2023-04-24 16:07:17 +0530

    slowely making progress in TS

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,9 @@
 console.log('hello world');
 let age = 21;
 console.log(age);
+const add = (a, b) => {
+    if (a < 5)
+        return "cannot add this";
+    return a += b;
+};
+console.log(add(10, 8));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 console.log('hello world');
 let age: number= 21;
 console.log(age);
+
+const add = (a:number, b:number)=>{
+    if(a<5)
+    return "cannot add this"
+    return a += b;
+};
+
+console.log(add(10,8));
