ed273e28bc4b21aafe66203902d156a15fd74ffb
Abhishek Kumar <abhishekkumar.in047@gmail.com>
2023-04-28 16:05:56 +0530

    ok day 3 done

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,11 @@
 "use strict";
-let firstName = 'pami';
-firstName = 'loggy';
-let lastName = 'Singh';
-let a = 'boom';
-a = 69;
-a = true;
-let b = 'hello';
-b = 69;
-b = ['hello', 1, 'world'];
-const run = (num, str = 'hello') => {
-    return num + str;
-};
-console.log(run(60));
-let student1 = {
-    name: 'Abhishek',
-    age: 21,
-    profession: 'developer',
-    num: 69
-};
-console.log(student1);
+let arr = ['hello', 'ji', 'sir ji'];
+console.log(typeof (arr));
+;
+let arr2 = [1, 2, 3, 4, 5];
+arr2.push(69);
+let tuples = [69, 'Abhishek Kumar', true];
+console.log(tuples);
+let graph = [44, 44];
+let [a, b] = graph;
+console.log([a, b]);
