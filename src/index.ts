11431aa7aa4de485ba34de02c4954694f6e0751f
Abhishek Kumar <abhishekkumar.in047@gmail.com>
2023-04-28 14:44:08 +0530

    this is Day 3

--- src/index.ts
+++ src/index.ts
@@ -11,39 +11,43 @@
 
 
 // day 2 * 
-let firstName: string = 'pami';
-firstName= 'loggy'      //i cannot give it a non-string value because it's type is explicit as string .
-// but if i haven't declared the type then it will again throw an error  
-let lastName = 'Singh'
-// lastName = 69;    //this is again throwing an error .
-let a:any = 'boom'
-a = 69;
-a = true;
+// let firstName: string = 'pami';
+// firstName= 'loggy'      //i cannot give it a non-string value because it's type is explicit as string .
+// // but if i haven't declared the type then it will again throw an error  
+// let lastName = 'Singh'
+// // lastName = 69;    //this is again throwing an error .
+// let a:any = 'boom'
+// a = 69;
+// a = true;
 
-let b: unknown= 'hello'
-b=69;
-b=['hello', 1, 'world']
+// let b: unknown= 'hello'
+// b=69;
+// b=['hello', 1, 'world']
 
-const run = (num:number, str:string= 'hello')=>{
-    return num + str
-};
-console.log(run(60))
+// const run = (num:number, str:string= 'hello')=>{
+//     return num + str
+// };
+// console.log(run(60))
 
-type Rule = {
-    name: String,
-    age: Number,
-    profession: String
-};
+// type Rule = {
+//     name: String,
+//     age: Number,
+//     profession: String
+// };
 
-type Favs = {
-    num: Number,
-}
+// type Favs = {
+//     num: Number,
+// }
 
-let student1:Rule & Favs = {
-    name: 'Abhishek',
-    age: 21,
-    profession: 'developer',
-    num:69
-};
-console.log(student1)
+// let student1:Rule & Favs = {
+//     name: 'Abhishek',
+//     age: 21,
+//     profession: 'developer',
+//     num:69
+// };
+// console.log(student1)
     
+
+//Day 3
+
+
